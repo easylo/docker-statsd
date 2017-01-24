8a63bb60fcbdd02644e39110012745d685c06557
Laurent RICHARD <easylo@gmail.com>
2017-01-24 17:37:15 +0100

    add STATSD_PORT

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
  {
     graphitePort: parseInt(process.env.GRAPHITE_PORT) || 2003,
     graphiteHost: process.env.GRAPHITE_HOST || "localhost",
-    port: 8125,
+    port:  parseInt(process.env.STATSD_PORT) || 8125,
     debug: process.env.DEBUG || false,
     dumpMessages: process.env.DEBUG || false,
     flushInterval: process.env.FLUSH || 10000,
